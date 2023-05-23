49c96e6c0828cd9097aa4b0783d4184958231248
mark mwendwa <mwendwamark310@gmail.com>
2023-05-23 13:47:39 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Mr.Chocolate',12,'M','Brown','Very tepmeramental',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Rowdy',12,'M','Brown','Very tepmeramental',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Tabitha',24,'F','Brown','tepmeramental',false);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',17,'M','Brown','Very Calm',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Melissa',15,'M','Brown','Calm',false);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Grinch',18,'M','Brown','tepmeramental',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ('Wendy',10,'M','Brown','Very tepmeramental',true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,NULL,NULL,NULL, NULL,NULL);
